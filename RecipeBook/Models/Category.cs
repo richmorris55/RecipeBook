0bf3c297c9e50d07a69899537dd224d2cb590e61
Richard Morris <richmorris55@yahoo.com>
2023-03-23 09:00:51 -0700

    InitialCommit

--- RecipeBook/Models/Category.cs
+++ RecipeBook/Models/Category.cs
@@ -4,7 +4,7 @@ namespace RecipeBook.Models
 	public class Category
 	{
         public int CategoryID { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
     }
 }
--- RecipeBook/Models/Recipes.cs
+++ RecipeBook/Models/Recipes.cs
@@ -3,33 +3,31 @@ namespace RecipeBook.Models
 {
 	public class Recipes
 	{
-		public Recipes()
-		{
-		}
+		
 
 		public int RecipeID { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Cuisine { get; set; }
+        public string? Cuisine { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public string TotalCookingTime { get; set; }
+        public string? TotalCookingTime { get; set; }
 
         public int Servings { get; set; }
 
-        public string IngredientsWithMeasurements { get; set; }
+        public string? IngredientsWithMeasurements { get; set; }
 
-        public string CookingInstructions { get; set; }
+        public string? CookingInstructions { get; set; }
 
-        public string NotesAndFAQs { get; set; }
+        public string? NotesAndFAQs { get; set; }
 
-        public string NutritionFacts { get; set; }
+        public string? NutritionFacts { get; set; }
 
         public int CategoryID { get; set; }
 
-        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Category>? Categories { get; set; }
 
 
     }
